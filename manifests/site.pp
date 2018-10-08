29c3c99470449ba0ff142985acb91921b935b471
knowever <knowever@gmail.com>
2018-10-08 11:41:11 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,9 @@
 node default {
-
+  file { '/root/README':
+    ensure => file,
+    content => 'This is a readme',
+    owner   => 'root',
+  }
 }
 node 'osboxes.home' {
   include role::master_server
