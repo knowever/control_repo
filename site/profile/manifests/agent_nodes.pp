a7e31f0b047dbd81c85976f4aa30318a5ef2ef00
knowever <knowever@gmail.com>
2018-10-21 16:20:03 -0400

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,4 +2,10 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent::node { 'web.puppet.vm': }
   dockeragent::node { 'db.puppet.vm': }
+
+range(1,5).each |$n| {
+    dockeragent::node { "agent${n}.${::fqdn}":
+        ports => ["${n}0080:80"],
+    }
+}
 }
